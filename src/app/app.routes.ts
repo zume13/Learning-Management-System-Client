ba7cbe246e74b782730f1fa8ee0f23285f859c88
cloud <ckgbasilio@gmail.com>
2026-07-25 16:35:42 +0800

    create: home page and link login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
+import { Home } from './features/home/pages/home/home';
 
 export const routes: Routes = [
   {
     path: '',
+    component: Home
+  },
+  {
+    path: 'login',
     component: Login
   }
 ];
--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
