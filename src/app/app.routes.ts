f16a5723538c85a143d6854640bb9610dbe7f9ce
Dustine <dustinesarmiento3@gmail.com>
2026-07-27 18:33:34 +0800

    Dashboard Sample

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: Dashboard
+    }
+];
--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink, CommonModule],
+  standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+
+  showDropdown = false;
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
+}
