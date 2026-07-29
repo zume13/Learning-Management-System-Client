3321e22500236eadbb1fcc9f06d86eacc16e61bb
cloud <ckgbasilio@gmail.com>
2026-07-29 21:23:09 +0800

    update: dashboard's todo design, link dashboard's todo to todo page

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CourseCard } from '../../../../shared/components/course-card/course-card';
+import { LucideArrowRight } from '@lucide/angular';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CourseCard],
+  imports: [ RouterLink, CourseCard, LucideArrowRight],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
