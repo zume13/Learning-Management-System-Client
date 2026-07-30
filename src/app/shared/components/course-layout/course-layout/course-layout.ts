5ac086dad3a451643e2a34dea199259f22bf7b3d
cloud <ckgbasilio@gmail.com>
2026-07-30 12:56:39 +0800

    course-layout

---
+++ src/app/shared/components/course-layout/course-layout/course-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-layout',
+  imports: [],
+  templateUrl: './course-layout.html',
+  styleUrl: './course-layout.css',
+})
+export class CourseLayout {}
