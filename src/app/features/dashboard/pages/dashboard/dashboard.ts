7bf39c6a8ecdcb7825cfd03790e029b4721e8b51
cloud <ckgbasilio@gmail.com>
2026-07-30 15:54:38 +0800

    update: relative header, add dashboard "+ add class", uniform background for todo, dashboard

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CourseCard } from '../../../../shared/components/course-card/course-card';
-import { LucideArrowRight } from '@lucide/angular';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ RouterLink, CourseCard, LucideArrowRight],
+  imports: [ RouterLink, CourseCard],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
