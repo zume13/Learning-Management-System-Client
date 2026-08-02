763f014ada15a4ab095d5ee9b3020e4c6e69d8c7
Dustine <dustinesarmiento3@gmail.com>
2026-08-02 14:42:12 +0800

    Course Tab Info

--- src/app/shared/components/course-nav-info/course-nav-info/course-nav-info.ts
+++ src/app/shared/components/course-nav-info/course-nav-info/course-nav-info.ts
@@ -6,4 +6,39 @@ import { Component } from '@angular/core';
   templateUrl: './course-nav-info.html',
   styleUrl: './course-nav-info.css',
 })
-export class CourseNavInfo {}
+export class CourseNavInfo {
+
+  courseCode = 'ABC123';
+
+  codeVisible = false;
+
+  toggleCodeVisibility() {
+    this.codeVisible = !this.codeVisible;
+  }
+
+  inviteLink = `https://localhost:4200/join/${this.courseCode}`;
+
+  linkCopied = false;
+  codeCopied = false;
+
+  copyInviteLink() {
+    navigator.clipboard.writeText(this.inviteLink);
+
+    this.linkCopied = true;
+
+    setTimeout(() => {
+      this.linkCopied = false;
+    }, 2000);
+  }
+
+  copyCourseCode() {
+    navigator.clipboard.writeText(this.courseCode);
+
+    this.codeCopied = true;
+    
+    setTimeout(() => {
+      this.codeCopied = false;
+    }, 2000);
+  }
+
+}
